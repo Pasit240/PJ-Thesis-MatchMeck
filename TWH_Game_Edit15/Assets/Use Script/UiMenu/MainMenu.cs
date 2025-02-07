3c82659c388780418c200ad852f9ede8207cd5c2
Ren7749 <159054824+Ren7749@users.noreply.github.com>
2025-02-07 22:18:21 +0700

    Pre for Cut Scene

--- TWH_Game_Edit15/Assets/Use Script/UiMenu/MainMenu.cs
+++ TWH_Game_Edit15/Assets/Use Script/UiMenu/MainMenu.cs
@@ -6,21 +6,25 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
-    {
-        SceneManager.LoadScene("Map01");
-        Time.timeScale = 1f;
+    {      
+        SceneManager.LoadScene("Map01");//ถ้ามี Cut Scene ใส่ชชื่อ Scene ที่มี Cut Scene แทนตรงในวงเล็บอันนี้
+        Time.timeScale = 1f;//อันนี้ไม่ต้องยุ่ง
+
+        //StartCoroutine(GameStart());//ถ้ามี Cut Scene เปิดอันนี้ด้วย
     }
 
-    public void LoadGame()
-    {
-        SceneManager.LoadScene("Map02");
-        Time.timeScale = 1f;
-    }
-
-    //public void Mainmenu()
+    //IEnumerator GameStart()
     //{
-    //    SceneManager.LoadScene("");
+    //    yield return new WaitForSeconds(1.0f);//อันนี้ใส่เวลา ใส่เท่ากับเวลาเล่น Cut Scene เลย ให้ใส่ในวงเล็บ
+    //    SceneManager.LoadScene("Map01");//อันนี้ไม่ต้องยุ่ง
+    //    Time.timeScale = 1f;//อันนี้ไม่ต้องยุ่ง
     //}
+
+    //public void LoadGame()
+    //{
+    //    SceneManager.LoadScene("Map02");
+    //    Time.timeScale = 1f;
+    //}   
 
     public void QuitGame()
     {
