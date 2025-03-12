d85bf02d8ba78f7c092a1b3d729fe82135346651
Pasit240 <pasitsittisat@gmail.com>
2025-03-12 19:08:00 +0700

    Edit map2

--- TWH_Game_Edit15/Assets/Scenes/qwer.cs
+++ TWH_Game_Edit15/Assets/Scenes/qwer.cs
@@ -8,13 +8,33 @@ public class qwer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(DelaySce(40f));
+        //StartCoroutine(DelaySce(40f));
+
+        
+    }
+
+    public void Update()
+    {
+        if (InputManager._isDebugOpen)
+        {
+            Skip();
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Skip();
+        }
     }
 
     IEnumerator DelaySce(float duration)
     {
         yield return new WaitForSeconds(duration);
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("Map01");
+    }
+
+    public void Skip()
+    {
+        SceneManager.LoadScene("Map01");
     }
 
 }
